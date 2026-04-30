9c34d3a82a9a7cea7141770a7c9041e78acf4fe1
ronitrajfr <ronitrajofficial7@gmail.com>
2026-04-30 12:20:07 +0530

    feat: added new test cases and completed lexer for IDENTIFIERS AND KEYWORDS

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -54,7 +54,16 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
+
 	l.readChar()
 	return tok
 }
@@ -62,4 +71,16 @@ func (l *Lexer) NextToken() token.Token {
 // newToken is a helper that builds and returns a Token from a type and a character
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
